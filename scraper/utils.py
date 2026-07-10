7482effde9a8a61608c25620e280e96c53da73e1
angelet-dev <tymoshchuk.work.k@gmail.com>
2026-07-10 21:59:48 +0200

    feat(utils): add function for dumping json API response.

---
+++ scraper/utils.py
@@ -0,0 +1,49 @@
+import json
+import logging
+
+
+def dump_api_response(response: str, path: str) -> bool:
+    """
+    Dump in JSONL file raw data from API response.
+    Returns True if successful, False otherwise.
+    """
+
+    if not response:
+        logging.warning("Received empty or None response. Skipping.")
+        return False
+
+    try:
+        dict_response = (
+            json.loads(response).get("data", {}).get("clientCompatibleListings", {})
+        )
+        response_keys = dict_response.keys()
+    except Exception as e:
+        logging.error(f"Failed to parse JSON structure. Error {type(e).__name__}: {e}")
+        return False
+
+    for key in response_keys:
+        match key:
+            case "error":
+                logging.error(f"API returned an error: {dict_response.get('error')}")
+
+            case "metadata":
+                logging.info("This response contains metadata.")
+
+            case "data":
+                try:
+                    ads = dict_response.get("data", [])
+
+                    with open(path, "a", encoding="utf-8") as f:
+                        for ad in ads:
+                            json_string = json.dumps(ad, ensure_ascii=False)
+                            f.write(json_string + "\n")
+
+                    logging.debug(f"Successfully dumped {len(ads)} ads.")
+                except Exception as e:
+                    logging.error(f"File write error {type(e).__name__}: {e}")
+                    return False
+
+            case _:
+                logging.warning(f"Unexpected key found in response: {key}")
+
+    return True
