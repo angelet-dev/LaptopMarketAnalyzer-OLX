d1c42cf227ce70d242a4a5eab0bdd20b4fab4fe9
angelet-dev <tymoshchuk.work.k@gmail.com>
2026-07-10 22:09:39 +0200

    refactor(api_client): transform API fetching to async
    - Rename fetch_api_catalog to api_query
    - Strip dumping logic (moved to scraper_utils)
    - Rewrite with httpx for asynchronous execution

--- scraper/api_client.py
+++ scraper/api_client.py
@@ -1,19 +1,27 @@
 import requests
 import logging
-import json 
-import time 
+import httpx
 import random
-from pathlib import Path
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%H:%M:%S'
-)
+import asyncio
+from scraper.utils import dump_api_response
 
 # Some basic API things
 
 url = "https://www.olx.pl/apigateway/graphql"
+
+
+query_total_elements = """
+query ListingSearchQuery($searchParameters: [SearchParameter!] = []) {
+  clientCompatibleListings(searchParameters: $searchParameters) {
+    ... on ListingSuccess {
+      metadata {
+        total_elements
+        visible_total_count
+      }
+    }
+  }
+}
+"""
 
 query = """
 query ListingSearchQuery($searchParameters: [SearchParameter!] = []) {
@@ -77,15 +85,15 @@ query ListingSearchQuery($searchParameters: [SearchParameter!] = []) {
 payload = {
     "query": query,
     "variables": {
-  "searchParameters": [
-    { "key": "query", "value": "macbook pro 13 m1" },
-    { "key": "price:from", "value": "1500" },
-    { "key": "price:to", "value": "2500" },
-    { "key": "state", "value": "used" },
-    { "key": "offset", "value": "120" },
-    { "key": "limit", "value": "50" }
-  ]
-}
+        "searchParameters": [
+            {"key": "query", "value": "macbook pro 13 m1"},
+            {"key": "price:from", "value": "1500"},
+            {"key": "price:to", "value": "2500"},
+            {"key": "state", "value": "used"},
+            {"key": "offset", "value": "120"},
+            {"key": "limit", "value": "50"},
+        ]
+    },
 }
 
 
@@ -96,51 +104,56 @@ headers = {
     "referer": "https://www.olx.pl/",
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
     "x-client": "DESKTOP",
-    "authorization": "ANONYMOUS" 
+    "authorization": "ANONYMOUS",
 }
 
 
-def fetch_api_catalog(api_url: str, payload: dict, header: dict, TIME_OUT = 10):
+async def api_query(api_url: str, payload: dict, TIME_OUT=10, **kwargs) -> str:
     """
-    Fetch raw advertisement data from the OLX API via a POST request and append it to a JSONL file.
+    Makes an API query and returns response text.
     """
+    try:
+        await asyncio.sleep(random.random() * 2)
 
-    try:         
-        time.sleep(random.randint(1,5))
-
-        response = requests.post(api_url, json=payload, headers = header, timeout = TIME_OUT)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                api_url, json=payload, timeout=TIME_OUT, **kwargs
+            )
 
         if response.status_code == 403:
-            logging.warning(f"Access forbidden (403) for {url}. Maybe IP is blocked.")
+            logging.warning(
+                f"Access forbidden (403) for {api_url}. Maybe IP is blocked."
+            )
             return None
         elif response.status_code == 429:
             logging.warning("Too many requests (429). Need increased pause!")
             return None
-        
+
         response.raise_for_status()
-        
-        if "error" in response.text:
-            api_error_output = response.json()["data"]["clientCompatibleListings"]["error"]
-            logging.warning(f"API return error: {api_error_output}")
-            return api_error_output["code"]
-        
-        ads = response.json()["data"]["clientCompatibleListings"]["data"]
+        return response.text
 
-        with open('data/raw/1.jsonl','a',encoding='utf-8') as f:
-            for ad in ads:
-                json_string = json.dumps(ad, ensure_ascii=False)
-                f.write(json_string + '\n')
-
-        return True
-
-    except requests.exceptions.HTTPError as e:
-        logging.error(f"HTTP error for {api_url}: {e}")
+    except requests.exceptions.Timeout:
+        logging.error(f"Timeout ({TIME_OUT} s) on loading {api_url}")
     except requests.exceptions.ConnectionError:
         logging.error(f"Network connection error while attempting to open {api_url}")
-    except requests.exceptions.Timeout:
-        logging.error(f"Timeout (10 s) on loading {api_url}")
+    except requests.exceptions.HTTPError as e:
+        logging.error(f"HTTP error for {api_url}: {e}")
     except Exception as e:
-        logging.error(f"Unpredictable error: {e}", exc_info=True)
-        
+        logging.error(f"Unexpected error {type(e).__name__}: {e}", exc_info=True)
+
     return None
 
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(funcName)s(): %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        force=True,
+    )
+
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("httpcore").setLevel(logging.WARNING)
+
+    response = asyncio.run(api_query(url, payload, headers=headers))
+    dump_api_response(response, "data/raw/1.jsonl")
